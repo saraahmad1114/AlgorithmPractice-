f2ffafc5fe11b44161cbc66cec34752e085688a5
Sara <saraahmad1114@yahoo.com>
2017-03-15 13:26:37 -0400

    done with the second question

--- AlgorithmPractice2.playground/Contents.swift
+++ AlgorithmPractice2.playground/Contents.swift
@@ -3,3 +3,11 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+func findIfStringsAreUnique (firstString: String)-> Bool{
+    return firstString.characters.count == Set(firstString.characters).count
+}
+
+//The idea behind this algorithm solution was that if a string is given then calling .character would break it down considering every character and then using .count to look at the number of characters. When you create a Set, by default all the duplicates are deleted so to compare the number of characters in the regular string to the number of strings in a set would indicated whether there are duplicates or not. 

5a5b36da057154da1d3b263193f547db5b56f9d3
Sara <saraahmad1114@yahoo.com>
2017-03-18 18:35:49 -0400

    another question

---
+++ LoopPractice.playground/Contents.swift
@@ -0,0 +1,43 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var word = "cheese"
+
+var wordArray = [String]()
+
+
+var count = 0
+
+var vowels = ["a", "e", "i", "o", "u"]
+
+
+var results = [(String, Int)]()
+
+
+for (index, char) in word.characters.enumerated() {
+    //print(String(char).capitalized, index)
+    if vowels.contains(String(char).lowercased()) {
+        results.append((String(char), index))
+        print(results)
+    }
+}
+
+
+var previous = 0
+for (i, n) in results {
+    if n - previous == 1 {
+        count += 1
+    }
+    previous = n
+}
+
+
+print(count)
+
+    
+
+
+

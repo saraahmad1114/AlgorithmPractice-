09bf8ce4bfbadc6906a5900bfc78dcdc6d1a3715
Sara <saraahmad1114@yahoo.com>
2017-03-18 19:26:01 -0400

    almost done with question

--- FourthQuestion.playground/Contents.swift
+++ FourthQuestion.playground/Contents.swift
@@ -3,3 +3,39 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Given two strings,write a method to decide if one is a permutation of the other.
+func createFrequencyDictionary (word: String)-> ([String: Int]){
+    var dictionary = [String: Int]()
+    for singleLetter in word.characters{
+        var value = 0
+        if dictionary.contains(where: singleLetter){
+            value = dictionary[String(singleLetter)]!
+            value = value + 1
+        }
+        else{
+            value = 1
+        }
+    }
+    return dictionary
+}
+
+func findPermutations(stringOne: String, stringTwo: String) -> Bool{
+    
+    var isPermutation: Bool = true
+    if stringOne.characters.count != stringTwo.characters.count {
+        isPermutation = false
+    }
+    else {
+        var stringOneDictionary = createFrequencyDictionary(word: stringOne)
+        var stringTwoDictionyar = createFrequencyDictionary(word: stringTwo)
+        if stringOneDictionary == stringTwoDictionyar{
+            isPermutation = true
+        }
+        else {
+            isPermutation = false
+        }
+    }
+
+    return isPermutation
+}

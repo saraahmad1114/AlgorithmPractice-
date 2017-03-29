3856110c0b7e41ad8ae8a47b710bb6860b835661
Sara <saraahmad1114@yahoo.com>
2017-03-28 22:42:19 -0400

    added question

--- 5.playground/Contents.swift
+++ 5.playground/Contents.swift
@@ -3,3 +3,8 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question: Write a method to replace all spaces in a string with "20%". You may assume that the string length of the string has sufficient space at the end to hold the additional characters, and the at you are given the "true" length of the string. 
+
+//input: "Mr. John Smith "
+//ouput: "Mr.%20John%20Smith"

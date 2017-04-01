de64cc2f72f779ed6ce134337a8d3141fb2363cd
Sara <saraahmad1114@yahoo.com>
2017-04-01 19:58:14 -0400

    almost done

--- 6.playground/Contents.swift
+++ 6.playground/Contents.swift
@@ -5,3 +5,21 @@ import UIKit
 var str = "Hello, playground"
 
 //Question: Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearragement of letters. The palindrome does not need to be limited to just dictionary words. 
+
+
+func ifPermutationOfPalindrome(firstString: String, secondString: String)-> Bool{
+
+    var newFirstString = firstString
+    var isPermutationOfPalindrome = true
+    var reversedVersion = String(firstString.characters.reversed())
+    
+    if newFirstString == reversedVersion
+    {
+        
+    }
+    else{
+        print("\(firstString) is not a palindrome")
+    }
+
+    return true
+}

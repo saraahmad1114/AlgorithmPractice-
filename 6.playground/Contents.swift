46f70294b4e3a0628186f19c37dc623c41d9c9c9
Sara <saraahmad1114@yahoo.com>
2017-04-04 20:57:33 -0400

    almost done

--- 6.playground/Contents.swift
+++ 6.playground/Contents.swift
@@ -6,6 +6,22 @@ var str = "Hello, playground"
 
 //Question: Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearragement of letters. The palindrome does not need to be limited to just dictionary words. 
 
+func createFrequencyDictionary (word: String)-> ([String: Int]){
+    var dictionary = [String: Int]()
+    for singleLetter in word.characters{
+        var value = 0
+        var stringSingleLetter = String(singleLetter)
+        if dictionary[stringSingleLetter] == nil {
+            value = dictionary[String(singleLetter)]!
+            value = value + 1
+        }
+        else{
+            value = 1
+        }
+    }
+    return dictionary
+}
+
 
 func ifPermutationOfPalindrome(firstString: String, secondString: String)-> Bool{
 
@@ -15,7 +31,7 @@ func ifPermutationOfPalindrome(firstString: String, secondString: String)-> Bool
     
     if newFirstString == reversedVersion
     {
-        //create a dictionary to see if the dictionaries are the same, then you have
+        
     }
     else{
         isPermutationOfPalindrome = false

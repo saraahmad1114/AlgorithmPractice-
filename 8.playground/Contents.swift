e1a589f50455f9141dc2896252ca085afa78910a
Sara <saraahmad1114@yahoo.com>
2017-04-10 01:34:04 -0400

    started question

--- 8.playground/Contents.swift
+++ 8.playground/Contents.swift
@@ -3,3 +3,15 @@
 import UIKit
 
 var str = "Hello, playground"
+//Question: Implement a method to perform basic string compression using the sounts of repeated characters. For example the string aabcccccaaa would become a2b1c5a3. If the compressed string would not become smaller than the orginal string, your method should return the original string. You can assume the string has only uppercase and lowercase letters. 
+
+//Create a dictionary and concatenate the contents to a single string
+
+func createACompressedString(givenString: String) -> String{
+
+
+
+
+
+    return "happy"
+}

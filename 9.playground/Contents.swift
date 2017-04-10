5e5fa3bbe5dd64462fbcf63020765b30e7ce7e56
Sara <saraahmad1114@yahoo.com>
2017-04-10 02:38:04 -0400

    added another example

--- 9.playground/Contents.swift
+++ 9.playground/Contents.swift
@@ -3,3 +3,7 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+

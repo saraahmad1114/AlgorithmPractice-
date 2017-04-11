9766100ff4c974c7c12391ab6e6e5dd8f9c6c3ac
Sara <saraahmad1114@yahoo.com>
2017-04-11 16:39:23 -0400

    done

--- 10.playground/Contents.swift
+++ 10.playground/Contents.swift
@@ -3,3 +3,23 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question: Asssume you have a method isSubstring which checks if one word is a substring of another. Given two strings s1 and s2, write code to check if s2 is a rotaton of s1 using only one call to isSubstring (e.g., "waterbottle" is a rotation of "erbottlewat")
+
+func isSubstring(s1: String, s2: String) -> Bool{
+
+    var isSubString = true
+
+    if s1.characters.count != s2.characters.count{
+        isSubString = false
+    }
+    else{
+        var s1s1 = s1 + s1
+        if s1s1.contains(s2){
+            isSubString = true
+        }
+    }
+    return isSubString
+}
+
+isSubstring(s1: "waterbottle", s2: "erbottlewat")

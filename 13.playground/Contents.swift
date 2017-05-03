0b4e86985100c1777bae9f0a2861efa2d9d95b3c
Sara <saraahmad1114@yahoo.com>
2017-05-03 14:02:03 -0400

    another algorithm question

--- 13.playground/Contents.swift
+++ 13.playground/Contents.swift
@@ -3,3 +3,27 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question: Reverse the characters in a a string 
+
+func reverseString(input: String) -> [Character]{
+
+    var reversedString = [Character]()
+    
+    for char in input.characters{
+        reversedString.insert(char, at: 0)
+    }
+
+    return reversedString
+}
+
+reverseString(input: "Sara")
+
+//another easier way to reverse the contents of a string are: 
+
+func reverseStringUsingBuiltInFunction(input: String) -> String{
+    
+    return String(input.characters.reversed())
+}
+
+//the above will utilize the built in reverse() function 

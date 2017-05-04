2dce9fec93beaaa7b761491458bdb8ac48035d4d
Sara <saraahmad1114@yahoo.com>
2017-05-04 18:17:34 -0400

    changs

---
+++ 14.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Question: add two numbers up which are presented in the form of a linked list 
+
+

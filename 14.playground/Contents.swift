52c17b7638f117677b6e179df68a085a13d281f4
Sara <saraahmad1114@yahoo.com>
2017-05-08 00:43:16 -0400

    added half of the answer

--- 14.playground/Contents.swift
+++ 14.playground/Contents.swift
@@ -4,6 +4,28 @@ import UIKit
 
 var str = "Hello, playground"
 
-//Question: add two numbers up which are presented in the form of a linked list 
+//Question: The prime factors of 13195 are 5, 7, 13 and 29.
+
+//What is the largest prime factor of the number 600851475143 ?
+
+
+//With recursion
+
+var currentPrime = 2
+
+func findLargestPrimeFactorOf(number: Int) -> Int {
+    if number <= 1 {
+        return currentPrime
+    }
+    
+    else if number % currentPrime == 0 {
+        return findLargestPrimeFactorOf(number: number / currentPrime)
+    } else {
+        currentPrime += 1
+        return findLargestPrimeFactorOf(number: number)
+    }
+}
+
+findLargestPrimeFactorOf(number: 600851475143)
 
 

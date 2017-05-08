d417abe7802b936db18bb8ece4897f199f1521a4
Sara <saraahmad1114@yahoo.com>
2017-05-08 14:28:45 -0400

    done

--- 16.playground/Contents.swift
+++ 16.playground/Contents.swift
@@ -6,3 +6,45 @@ var str = "Hello, playground"
 
 //Question: reversing a string without creating an additional array or other data structure 
 
+func reverseString (inputString: String) -> String{
+
+    var reversedString = ""
+    
+    if inputString.characters.count == 2 {
+        reversedString = inputString
+        return reversedString
+    }
+
+   
+        var characterArray = Array(inputString.characters)
+        var leftIndex = 0
+        var rightIndex = inputString.characters.count - 1
+        
+        
+        while leftIndex < rightIndex {
+        
+           var leftElement = characterArray[leftIndex]
+           var rightElement = characterArray[rightIndex]
+            
+           characterArray.remove(at: leftIndex)
+           //print(characterArray.remove(at: leftIndex))
+            
+           characterArray.insert(rightElement, at: leftIndex)
+           //print(characterArray.insert(rightElement, at: leftIndex))
+            
+           characterArray.remove(at: rightIndex)
+           //print(characterArray.remove(at: rightIndex))
+            
+           characterArray.insert(leftElement, at: rightIndex)
+           //print(characterArray.insert(leftElement, at: rightIndex))
+            
+        leftIndex += 1
+        rightIndex -= 1
+            
+        }
+
+    
+    return String(characterArray)
+}
+
+reverseString(inputString: "characters")

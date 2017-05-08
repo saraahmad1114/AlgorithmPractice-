fba32cef20d9f235a0d05181815feb64279f628c
Sara <saraahmad1114@yahoo.com>
2017-05-08 14:48:12 -0400

    done

--- 17.playground/Contents.swift
+++ 17.playground/Contents.swift
@@ -3,3 +3,18 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question: Given an input of a radius, return the area of a circle.  
+//Formula equals to area = pie(r^2)
+
+func findAreaOfCircle (radius: Double) -> Double{
+
+    var areaOfCircle = Double()
+    
+    areaOfCircle = 3.14 * pow(radius, Double(2))
+    
+    return areaOfCircle
+
+}
+
+findAreaOfCircle(radius: 3.15)

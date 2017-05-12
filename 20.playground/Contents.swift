e31b4a6e6c6eba343ad935b01e20a8fd90a088fd
Sara <saraahmad1114@yahoo.com>
2017-05-11 23:02:02 -0400

    added notes

--- 20.playground/Contents.swift
+++ 20.playground/Contents.swift
@@ -6,4 +6,10 @@ var str = "Hello, playground"
 
 //Question: There are three types of edits that can be performed on strings: insert a character remove a character, or replace a character. Given two strings, write a function to check if they are one edit or zero edits away. 
 
+//insert a character - meaning an extra character exists so its length = length + 1
+
+//remove a character - meaning one character in length is missing so its length = length - 1 
+
+//replace a string - meaning one character that so the length = length 
+
 

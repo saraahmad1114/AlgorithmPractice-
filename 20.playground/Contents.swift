60616bd2002204f40d4c037899839ffde41261ff
Sara <saraahmad1114@yahoo.com>
2017-05-12 00:02:20 -0400

    added answer

--- 20.playground/Contents.swift
+++ 20.playground/Contents.swift
@@ -13,3 +13,52 @@ var str = "Hello, playground"
 //replace a string - meaning one character that so the length = length 
 
 
+func oneEditAway (str1: String, str2: String) -> Bool{
+
+    //inserted a character
+    if str1.characters.count == str2.characters.count + 1 || str2.characters.count == str1.characters.count + 1 {
+        return insertedACharacter(str1: str1, str2: str2)
+    }
+    //removed a character
+    else if str1.characters.count == str2.characters.count - 1 || str2.characters.count == str1.characters.count - 1{
+        return removedACharacter(str1, str2)
+    }
+    //replaced a character
+    else if str1.characters.count == str2.characters.count {
+        return replacedACharacter(str1, str2)
+    }
+    //else just return false
+    else{
+        return false
+    }
+}
+
+func insertedACharacter(str1: String, str2: String) -> Bool{
+    
+    var differenceCounter = 0
+    
+    var str1Array: [String] = [String(str1.characters)]
+    var str2Array: [String] = [String(str2.characters)]
+
+    for i in 0...str1Array.count-1{
+        if str1Array[i] == str2Array[i]{
+            differenceCounter += 1
+        }
+    }
+    if differenceCounter >= 1 {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+func removedACharacter(str1: String, str2: String) -> Bool{
+
+    
+
+}
+
+
+
+

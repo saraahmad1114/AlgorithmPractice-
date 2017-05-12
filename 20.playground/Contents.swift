ff421c53d4f791c7d9f6dd869afc5de2ce3b650c
Sara <saraahmad1114@yahoo.com>
2017-05-11 22:56:30 -0400

    added question

--- 20.playground/Contents.swift
+++ 20.playground/Contents.swift
@@ -4,4 +4,6 @@ import UIKit
 
 var str = "Hello, playground"
 
+//Question: There are three types of edits that can be performed on strings: insert a character remove a character, or replace a character. Given two strings, write a function to check if they are one edit or zero edits away. 
+
 

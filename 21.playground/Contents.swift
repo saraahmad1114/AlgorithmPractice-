4e71066ae10ac8e092d005c584bb6d46e4287368
Sara <saraahmad1114@yahoo.com>
2017-05-17 16:05:59 -0400

    added question

--- 21.playground/Contents.swift
+++ 21.playground/Contents.swift
@@ -3,3 +3,8 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question: Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2b1c5a3. If the compressed string would not become smaller thatn the original string, your method should return the original string. You can assume the sring has only uppercase and lowercase letters. 
+
+
+

27bea1c73d36507c9186ab1e15b0a47495727c41
Sara <saraahmad1114@yahoo.com>
2017-05-21 18:52:41 -0400

    keep trying

--- 20.playground/Contents.swift
+++ 20.playground/Contents.swift
@@ -32,12 +32,12 @@ func oneEditAway (str1: String, str2: String) -> Bool{
     //removal of a character
     else if str1.characters.count - 1  == str2.characters.count {
         print("character has been removed")
-        return differenceInCharacter(str1: str1, str2: str2)
+        return removalDifferenceInCharacter(str1: str1, str2: str2)
     }
     //replacement of a character
     else if str1.characters.count == str2.characters.count {
         print("character has been replaced")
-        return differenceInCharacter(str1: str1, str2: str2)
+        return removalDifferenceInCharacter(str1: str1, str2: str2)
         
     }
     else{
@@ -45,9 +45,36 @@ func oneEditAway (str1: String, str2: String) -> Bool{
     }
 }
 
+func removalDifferenceInCharacter(str1: String, str2: String)-> Bool{
+    
+    var difference = true
+    
+    var str1Counter = 0
+    var str2Counter = 0
+    
+    var str1Array: [String] = [String(str1.characters)]
+    var str2Array: [String] = [String(str2.characters)]
+    
+    for i in 0...str1Array.count-1{
+        if str1Array[i] == str2Array[i] {
+            str1Counter += 1
+            str2Counter += 1
+        }
+    }
+    if str2Counter == str1Array.count-1 {
+        difference = true
+    }
+        
+    else if str2Counter < str1Array.count-1{
+        difference = false
+    }
+    return difference
+}
+
 
 func differenceInCharacter(str1: String, str2: String) -> Bool{
 
+    var differenceInChar = true
     var differenceCounter = 0
     
     var str1Array: [String] = [String(str1.characters)]
@@ -59,18 +86,18 @@ func differenceInCharacter(str1: String, str2: String) -> Bool{
         }
     }
     if differenceCounter == 1 {
-        return true
+        differenceInChar = true
     }
     
-    else{
-        return false
+    else if differenceCounter >= 2 {
+        differenceInChar = false
     }
 
-
+    return differenceInChar
 }
 
 
-print(oneEditAway(str1: "pale", str2: "baejkjljljlj"))
+print(oneEditAway(str1: "pale", str2: "bake"))
 
 
 
--- 21.playground/Contents.swift
+++ 21.playground/Contents.swift
@@ -6,7 +6,41 @@ var str = "Hello, playground"
 
 //Question: Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2b1c5a3. If the compressed string would not become smaller thatn the original string, your method should return the original string. You can assume the sring has only uppercase and lowercase letters. 
 
-//aabcccccaaa = a2b1c5a3 
+
+
+
+
+func compressAString(input: String) -> String{
+
+    var compressedString = ""
+    
+    var counter = 0
+    
+    for i in 0...input.characters.count-1 {
+        
+        counter += 1
+        
+        var firstChar = String(input[input.index(input.startIndex, offsetBy: i)])
+        
+        var secondChar = String(input[input.index(input.startIndex, offsetBy: i+1)])
+        
+        if firstChar != secondChar {
+        
+            counter += 1
+            
+            compressedString += "\(firstChar)\(counter)"
+        }
+        
+    }
+    
+    //result.characters.count >= str.characters.count ? str : result
+
+    return compressedString
+
+}
+
+compressAString(input: "aabcccccaaa")
+
 
 
 

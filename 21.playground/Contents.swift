36e174853fa77298ccbd3098b9dddece3ae5af0a
Sara <saraahmad1114@yahoo.com>
2017-05-21 20:12:29 -0400

    changes

--- 21.playground/Contents.swift
+++ 21.playground/Contents.swift
@@ -7,39 +7,47 @@ var str = "Hello, playground"
 //Question: Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2b1c5a3. If the compressed string would not become smaller thatn the original string, your method should return the original string. You can assume the sring has only uppercase and lowercase letters. 
 
 
+func compressAString(input: String) -> String{
+
+    var compressedString = ""
+    
+    var counter = 0
+    
+    for i in 0...input.characters.count-1 {
+        
+        var firstChar = String(input[input.index(input.startIndex, offsetBy: i)])
+        
+        var secondChar = String(input[input.index(input.startIndex, offsetBy: i+1)])
+        
+        if firstChar == secondChar {
+        
+            counter += 1
+            
+        }
+        else {
+        
+            compressedString += "\(firstChar)\(counter)"
+            
+            secondChar = firstChar
+            
+            counter = 1
+        
+        }
+        
+    }
+
+
+    return compressedString
+
+}
+
+var input = "aabcccccaaa"
 
 
 
-//func compressAString(input: String) -> String{
-//
-//    var compressedString = ""
-//    
-//    var counter = 0
-//    
-//    for i in 0...input.characters.count-1 {
-//        
-//        counter += 1
-//        
-//        var firstChar = String(input[input.index(input.startIndex, offsetBy: i)])
-//        
-//        var secondChar = String(input[input.index(input.startIndex, offsetBy: i+1)])
-//        
-//        if firstChar != secondChar {
-//        
-//            counter += 1
-//            
-//            compressedString += "\(firstChar)\(counter)"
-//        }
-//        
-//    }
-//    
-//    //result.characters.count >= str.characters.count ? str : result
-//
-//    return compressedString
-//
-//}
-//
-//compressAString(input: "aabcccccaaa")
+
+
+
 
 
 

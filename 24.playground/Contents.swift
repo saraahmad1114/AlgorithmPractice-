c33cb937f0273bf075325f64ca90bb79ccf53142
Sara <saraahmad1114@yahoo.com>
2017-05-24 22:42:13 -0400

    added questions

--- 24.playground/Contents.swift
+++ 24.playground/Contents.swift
@@ -5,3 +5,6 @@ import UIKit
 var str = "Hello, playground"
 
 //Question: Write code to remove the duplicates from an unsorted linked list 
+
+//The idea is to have a set dataset in order to prevent any duplicates from being added 
+

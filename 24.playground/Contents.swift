253d47fff6a2aaae0b308cd4ee4c255de545a623
Sara <saraahmad1114@yahoo.com>
2017-05-26 00:14:19 -0400

    added changes

--- 24.playground/Contents.swift
+++ 24.playground/Contents.swift
@@ -72,3 +72,5 @@ print(h)
 
 print(removeDuplicates(head: h))
 
+
+
--- 25.playground/Contents.swift
+++ 25.playground/Contents.swift
@@ -4,6 +4,72 @@ import UIKit
 
 var str = "Hello, playground"
 
-//Question: Implement an algorithm to find the kth to last element of a singly linked list 
+//Another way to solve question number 24, using the Ray Wenderlich tutorial
+
+
+//This clasw will define what makes a Node
+public class Node{
+
+    var value: String
+    
+    var next: Node?
+    
+    weak var previous: Node?
+    
+    init(value: String){
+        self.value = value
+    }
+}
+
+
+//This class will allow you to keep track of where the list begins and ends 
+public class linkedList {
+
+    fileprivate var head: Node?
+    private var tail: Node?
+    
+    public var isEmpty: Bool{
+        return head == nil
+    }
+    
+    public var first: Node? {
+        return head
+    }
+    
+    public var last: Node? {
+        return tail
+    }
+    
+    //To handle appending a new node on your list 
+    public func add (value: String){
+    
+        let newNode = Node(value: value)
+        
+        if let tailNode = tail {
+            newNode.previous = tailNode
+            tailNode.next = newNode
+        }
+        
+        else {
+            head = newNode
+        }
+        
+        tail = newNode
+    }
+}
+
+//creating a linkedList
+let dogBreeds = linkedList()
+
+
+//adding to the linkedList
+dogBreeds.add(value: "Labrador")
+dogBreeds.add(value: "Bulldog")
+dogBreeds.add(value: "Beagle")
+dogBreeds.add(value: "Husky")
+dogBreeds.add(value: "Husky")
+
+let setDogBreeds: Set<String> = []
+
 
 

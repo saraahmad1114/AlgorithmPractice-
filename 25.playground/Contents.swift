9fd70fb7c514d058411556cf58e56c8fa5fee182
Sara <saraahmad1114@yahoo.com>
2017-05-29 22:53:18 -0400

    part of the linked LIst

--- 25.playground/Contents.swift
+++ 25.playground/Contents.swift
@@ -6,3 +6,25 @@ var str = "Hello, playground"
 
 //Question: Implement an algorithm to find the kth to last element of a singly linked list. 
 
+
+//definition of the Linked List 
+class Node: CustomStringConvertible{
+    
+    var value: Int
+    var next: Node?
+    
+    var description: String{
+        
+        if next != nil {
+            return "(\(value) \(next))"
+        }
+        else {
+            return "(\(value) \(next))"
+        }
+        
+    }
+    init(value: Int) {
+        self.value = value
+    }
+    
+}

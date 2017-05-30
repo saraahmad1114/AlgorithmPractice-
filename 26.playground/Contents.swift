683f16beda2efe22587168fc0a16cfbfbcf10828
Sara <saraahmad1114@yahoo.com>
2017-05-30 17:58:36 -0400

    added a question

--- 26.playground/Contents.swift
+++ 26.playground/Contents.swift
@@ -3,3 +3,7 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question: Implement an algorithm to delete a node in the middle (any node but the first and last node, not necessarily the exact middle) of a singly linked list, given only access to that node. 
+
+

fbc12babad7e33ef610bf8d58b4273a38fae1ae0
Sara <saraahmad1114@yahoo.com>
2017-05-30 17:16:34 -0400

    done

--- 25.playground/Contents.swift
+++ 25.playground/Contents.swift
@@ -53,7 +53,7 @@ func generateList() -> Node {
 
 //3. Create a counter to count the items in the linked list
 
-func counter(h:Node) -> Int{
+func counter(h:Node?) -> Int{
     
     var current = h
     var counter = 0
@@ -62,30 +62,42 @@ func counter(h:Node) -> Int{
     while current != nil{
     
         counter += 1
-        current = current.next!
+        current = current!.next
         
     }
-    
 
     return counter
 }
 
-//4. 
+
+let firstList = generateList()
+
+print(firstList)
+
+let number = counter(h: firstList)
 
 
+//4. Print the kth element in the linked list
 
-func printKthtoLast (h: Node, k: Int) -> Int{
-
-    var head = h
+func printKthtoLast (head: Node?, k: Int) -> Int{
     
-    if h == nil {
+    var current = head
+    
+    if current == nil {
         return 0
     }
-    var index = printKthtoLast(h: h.next!, k: k) + 1
+    var index = printKthtoLast(head: current?.next, k: k) + 1
+    
     if index == k {
-        print("\(k)th to last node is \(h.value)")
+    
+        print("\(k) th to last node is \(head?.value)")
+    
     }
 
-        return index
+    return index
 }
+
+printKthtoLast(head: firstList, k: number)
+
+
 

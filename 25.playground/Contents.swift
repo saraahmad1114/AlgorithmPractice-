0c3efd1ee566a53c29d67b4c4d20dc13775225d8
Sara <saraahmad1114@yahoo.com>
2017-05-30 16:49:57 -0400

    almost done

--- 25.playground/Contents.swift
+++ 25.playground/Contents.swift
@@ -7,7 +7,8 @@ var str = "Hello, playground"
 //Question: Implement an algorithm to find the kth to last element of a singly linked list. 
 
 
-//definition of the Linked List
+//1. list class - Define the linked list
+
 class Node: CustomStringConvertible{
     
     var value: Int
@@ -29,18 +30,62 @@ class Node: CustomStringConvertible{
     
 }
 
-let h = Node.init(value: 1)
+//2. Create the list
 
-var current = h
-
-for i in 0..<30 {
+func generateList() -> Node {
     
-    let randomValue = Int(arc4random_uniform(10))
+    let h = Node.init(value: 1)
     
-    current.next = Node.init(value: randomValue)
+    var current = h
     
-    current = current.next!
+    for _ in 0..<30 {
+        
+        let randomValue = Int(arc4random_uniform(10))
+        
+        current.next = Node.init(value: randomValue)
+        
+        current = current.next!
+        
+    }
+    
+    return h
 }
 
-print(h)
+//3. Create a counter to count the items in the linked list
+
+func counter(h:Node) -> Int{
+    
+    var current = h
+    var counter = 0
+    
+    
+    while current != nil{
+    
+        counter += 1
+        current = current.next!
+        
+    }
+    
+
+    return counter
+}
+
+//4. 
+
+
+
+func printKthtoLast (h: Node, k: Int) -> Int{
+
+    var head = h
+    
+    if h == nil {
+        return 0
+    }
+    var index = printKthtoLast(h: h.next!, k: k) + 1
+    if index == k {
+        print("\(k)th to last node is \(h.value)")
+    }
+
+        return index
+}
 

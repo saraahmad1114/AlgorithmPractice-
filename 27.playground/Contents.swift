1d3b780123cbf0b9649ff3f4890f16f41c6ad77f
Sara <saraahmad1114@yahoo.com>
2017-05-30 19:39:19 -0400

    added question

--- 27.playground/Contents.swift
+++ 27.playground/Contents.swift
@@ -3,3 +3,7 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question:  Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x. If x is contained with the list, the values of x only need to be after the elements less than x (see below). The partition element x can appear anywhere in the "right partition"; it does not need to appear between left and right partitions. 
+
+

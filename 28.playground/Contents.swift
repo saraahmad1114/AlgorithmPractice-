064dbe9c640306435904d856cf70042abe6c5f52
Sara <saraahmad1114@yahoo.com>
2017-06-04 23:27:47 -0400

    first part

--- 28.playground/Contents.swift
+++ 28.playground/Contents.swift
@@ -6,5 +6,25 @@ var str = "Hello, playground"
 
 //Question: You have two numbers represented by a linked list, where each node contains a single digit. The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list. 
 
+class Node: CustomStringConvertible {
 
+    var value: Int
+    
+    var next: Node?
+    
+    var description: String{
+        if next == nil {
+            return "\(value) \(next)"
+        }
+        else {
+            return "\(value) \(next)"
+        }
+        
+    }
+
+    init(value: Int){
+        self.value = value
+    }
+
+}
 

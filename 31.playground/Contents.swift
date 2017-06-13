c7c8e55f067963922c8dad07555f43f2a22f7edb
Sara <saraahmad1114@yahoo.com>
2017-06-12 21:48:29 -0400

    added question

--- 31.playground/Contents.swift
+++ 31.playground/Contents.swift
@@ -3,3 +3,19 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question: Implement a function to check if a linked list is a palinderome 
+
+class Node {
+
+    var value: Int
+    
+    var next: Node?
+    
+    init(value: Int, next: Node?) {
+        self.value = value
+        self.next = next
+    }
+
+}
+

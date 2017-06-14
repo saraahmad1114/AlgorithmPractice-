41778a1d1cf314be0dbf5f4ae458cc6dbec54ba7
Sara <saraahmad1114@yahoo.com>
2017-06-13 21:04:27 -0400

    done with linear search

--- 32.playground/Contents.swift
+++ 32.playground/Contents.swift
@@ -7,16 +7,27 @@ var str = "Hello, playground"
 //Question: finding a value in an array of numbers, using the linear and binary search method 
 
 
-func searchingLinearly(value: Int, array: [Int]) -> Bool{
-    
+func linearSearchForValue(value: Int, array: [Int]) -> Bool{
     for num in array {
-    
         if num == value {
             return true
         }
     }
+    return false
+}
+
+linearSearchForValue(value: 2, array: [1, 4, 5, 87, 9, 90, 101, 56, 111])
+
+//returns false 
+
+linearSearchForValue(value: 111, array: [1, 4, 5, 87, 9, 90, 101, 56, 111])
+
+//returns true 
+
+func binarySearchForValue (value: Int, array: [Int]) -> Bool {
+
 
     return false
 }
 
-searchingLinearly(value: 111, array: [1, 4, 5, 87, 9, 90, 101, 56, 111])
+

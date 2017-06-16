eec54a684c7e8eb68420ee8ff021f499f704d805
Sara <saraahmad1114@yahoo.com>
2017-06-15 22:30:23 -0400

    stack

--- 33.playground/Contents.swift
+++ 33.playground/Contents.swift
@@ -3,3 +3,46 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Stacks - Understanding stacks and their implementation in Swift
+//Replacement Algorithm is LIFO
+
+struct Stack {
+    
+    fileprivate var array: [String] = []
+    
+    //You'll be interacting with the array to implement the push, pop, and peek methods
+
+    
+    mutating func push (_element: String){
+        array.append(_element)
+    }
+    
+    mutating func pop() -> String? {
+        return array.popLast()
+    }
+    
+    mutating func peek () -> String? {
+        return array.last
+    }
+
+}
+
+var rawBookStack = Stack()
+
+rawBookStack.push(_element: "FirstBook")
+
+rawBookStack.push(_element: "secondBook")
+
+rawBookStack.push(_element: "ThirdBook")
+
+rawBookStack.pop()
+//removed the last element inserted in
+
+rawBookStack.peek()
+//
+
+
+
+
+

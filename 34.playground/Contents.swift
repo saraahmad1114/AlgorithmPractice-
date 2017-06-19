01337013e6204d45229bbc1f61555ffe3f751891
Sara <saraahmad1114@yahoo.com>
2017-06-19 15:01:48 -0400

    done

--- 34.playground/Contents.swift
+++ 34.playground/Contents.swift
@@ -4,4 +4,22 @@ import UIKit
 
 var str = "Hello, playground"
 
-//Question:
+//Question: Write a function that accepts a String as its only parameter, and returns true if the string has only unique letters, taking letter case into account. 
+
+func isStringUnique (input: String) -> Bool {
+
+    return input.characters.count == Set(input.characters).count
+
+}
+
+isStringUnique(input: "AaBbCc")
+
+//returns true 
+
+isStringUnique(input: "hello")
+
+//returns false 
+
+isStringUnique(input: "No duplicates")
+
+//returns true 

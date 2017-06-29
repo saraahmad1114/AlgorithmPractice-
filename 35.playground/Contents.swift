74a6b8eb2c5f8b68c0f281c3772b0dc06f5ef563
Sara <saraahmad1114@yahoo.com>
2017-06-29 12:58:00 -0400

    done

--- 35.playground/Contents.swift
+++ 35.playground/Contents.swift
@@ -140,10 +140,51 @@ isNumberPrime(num: 16777259)
 
 //returns true 
 
-//
+//20. Create a function that accepts any positive integer and returns the next higher and next lowest number that has the same number of ones in its binary representation. If either number is not possible, return nil for it. 
+
+func findNextHigherAndLowerBinaryNum (number: Int) -> (nextHighest: Int?, nextLowest: Int?){
+
+    let targetBinary = String(number, radix:2)
+    let targetOnes = targetBinary.characters.filter { $0 == "1"}.count
+    
+    var nextHighest: Int? = nil
+    var nextLowest: Int? = nil
+    
+    for i in number + 1...Int.max{
+        
+        let currentBinary = String(i, radix:2)
+        
+        let currentOnes = currentBinary.characters.filter { $0 == "1"}.count
+        
+        if targetOnes == currentOnes {
+            nextHighest = i
+            break
+        }
+    }
+    
+    for i in (0..<number).reversed(){
+        
+        let currentBinary = String(i, radix: 2)
+        let currentOnes = currentBinary.characters.filter{$0 == "1"}.count
+        
+        if targetOnes == currentOnes{
+            nextLowest = i
+            break
+        }
+    }
+    
+return (nextHighest, nextLowest)
+
+}
 
 
+findNextHigherAndLowerBinaryNum(number: 12)
 
+//returns 17 and 10
+
+findNextHigherAndLowerBinaryNum(number: 28)
+
+//returns 35 and 26
 
 
 

c7a65ef7cb5ae6f63d741a5058b4232db046015a
Sara <saraahmad1114@yahoo.com>
2017-07-01 22:16:43 -0400

    added answer

--- 36.playground/Contents.swift
+++ 36.playground/Contents.swift
@@ -46,6 +46,29 @@ func returnURL() -> URL {
 
 //30. Question: Write a function that accepts a path to a directory and returns an array of all the JPEGs that have been created in the last 48 hours. 
 
+func challenge30(in directory: String) -> [String] {
+    
+    let fm = FileManager.default
+    
+    let directoryURL = URL(fileURLWithPath: directory)
+    
+    guard let files = try? fm.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil) else { return [] }
+    
+    var jpegs = [String]()
+    
+    for file in files {
+        if file.pathExtension == "jpg" || file.pathExtension == "jpeg" {
+            guard let attributes = try? fm.attributesOfItem(atPath: file.path) else { continue }
+            guard let creationDate = attributes[.creationDate] as? Date else { continue }
+            
+            if creationDate > Date(timeIntervalSinceNow: -60 * 60 * 48) {
+                jpegs.append(file.lastPathComponent)
+            }
+        }
+    }
+    
+    return jpegs
+}
 
 
 

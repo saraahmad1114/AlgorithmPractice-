ed6054a769438c485a3e5810e5db0a185343db7f
Sara <saraahmad1114@yahoo.com>
2017-07-02 14:36:44 -0400

    added question

--- 36.playground/Contents.swift
+++ 36.playground/Contents.swift
@@ -107,6 +107,35 @@ func acceptFile(filename: String, count: String) -> Int {
     return wordsSet.count(for: count)
 }
 
+//33. Write a function that accepts the name of a directory to scan, and returns an array of filenames that appear more than once in that directory or any of its subdirectories. Your function should scan all subdirectories, including subdirectories of subdirectories. 
+
+func scan(in directory: String) -> [String] {
+    
+    let fm = FileManager.default
+    let directoryURL = URL(fileURLWithPath: directory)
+    
+    guard let files = fm.enumerator(at: directoryURL, includingPropertiesForKeys: nil) else { return []}
+    var duplicates = Set<String>()
+    var seen = Set<String>()
+    
+    for case let file as URL in files {
+        
+        guard file.hasDirectoryPath == false else { continue }
+        
+        let filename = file.lastPathComponent
+        
+        if seen.contains(filename) {
+            duplicates.insert(filename)
+        }
+        
+        seen.insert(filename)
+    }
+    
+    return Array(duplicates)
+    
+}
+
+
     
 
 

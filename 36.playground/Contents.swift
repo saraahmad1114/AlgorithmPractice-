c9395eb44d12a18241df16e240c863559aad41a8
Sara <saraahmad1114@yahoo.com>
2017-07-01 21:33:55 -0400

    added answer

--- 36.playground/Contents.swift
+++ 36.playground/Contents.swift
@@ -6,3 +6,17 @@ var str = "Hello, playground"
 
 //Question: Write a function that accepts a filename on disk, then prints its last N lines in reverse order, all on a single line separated by commas 
 
+func challenge27(filename: String, lineCount: Int) {
+    
+    guard let input = try? String(contentsOfFile: filename) else { return }
+    
+    var lines = input.components(separatedBy: "\n")
+    guard lines.count > 0 else { return }
+    
+    lines.reverse()
+    
+    for i in 0 ..< min(lines.count, lineCount) {
+        print(lines[i])
+    }
+}
+

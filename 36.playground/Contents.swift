d3a94420a435d64f8143bb7fa3fa326de3de136f
Sara <saraahmad1114@yahoo.com>
2017-07-01 21:26:28 -0400

    added question

--- 36.playground/Contents.swift
+++ 36.playground/Contents.swift
@@ -3,3 +3,6 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Question: Write a function that accepts a filename on disk, then prints its last N lines in reverse order, all on a single line separated by commas 
+

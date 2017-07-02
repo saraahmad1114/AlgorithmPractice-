80ec83cb12446b84767d473ebeab49c0601f856c
Sara <saraahmad1114@yahoo.com>
2017-07-02 16:20:12 -0400

    done

--- 36.playground/Contents.swift
+++ 36.playground/Contents.swift
@@ -46,7 +46,7 @@ func returnURL() -> URL {
 
 //30. Question: Write a function that accepts a path to a directory and returns an array of all the JPEGs that have been created in the last 48 hours. 
 
-func challenge30(in directory: String) -> [String] {
+func returnAllJPEGs(in directory: String) -> [String] {
     
     let fm = FileManager.default
     
@@ -158,8 +158,27 @@ func scanDirectory (in directory: String) -> [String]{
 }
 
 
+//35. Question: Write a function that accepts a path to a directory, then converts to PNGs any JPEGs it finds in there, leaving the originals intact. If any JPEG can't be converted the function should just quietly continue. 
 
+func convertPNGsToJPEGs(in directory: String)
+{
+    let fm = FileManager.default
+    let directoryURL = URL(fileURLWithPath: directory)
+    guard let files = try? fm.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil) else { return }
     
+    for file in files {
+    guard file.pathExtension == "jpeg" || file.pathExtension == "jpg" else { continue}
+    guard let image = UIImage(contentsOfFile: file.path) else { continue }
+    guard let png = UIImagePNGRepresentation(image) else { continue }
+    
+    let newFilename = file.deletingPathExtension().appendingPathExtension("png")
+    _ = try? png.write(to: newFilename)
+    }
+}
+
+
+
+
 
 
 

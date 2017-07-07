8546a8a0115670f59d048c66c042c7654007b9aa
Sara <saraahmad1114@yahoo.com>
2017-07-07 19:35:17 -0400

    done

--- 37.playground/Contents.swift
+++ 37.playground/Contents.swift
@@ -138,7 +138,44 @@ extension Collection where Iterator.Element: Equatable {
 
 //43. Create a linked list of lowercase English alphabet letters, along with a method that traverses all nodes and print their letters on a single line separated by spaces. 
 
+class LinkedListNode<T> {
+    var value: T
+    var next: LinkedListNode?
+    
+    init(value: T) {
+        self.value = value
+    }
+}
 
+class LinkedList<T> {
+    var start: LinkedListNode<T>?
+    
+    func printNodes() {
+        var currentNode = start
+        
+        while let node = currentNode {
+            print(node.value, terminator: " ")
+            currentNode = node.next
+        }
+    }
+}
+
+var list = LinkedList<Character>()
+var previousNode: LinkedListNode<Character>? = nil
+
+for letter in "abcdefghijklmnopqrstuvwxyz".characters {
+    let node = LinkedListNode(value: letter)
+    
+    if let predecessor = previousNode {
+        predecessor.next = node
+    } else {
+        list.start = node
+    }
+    
+    previousNode = node
+}
+
+list.printNodes()
 
 
 

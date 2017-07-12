67418fc6d373ba19d530cb1e937b4bec34ba8506
Sara <saraahmad1114@yahoo.com>
2017-07-11 20:02:26 -0400

    done

--- 37.playground/Contents.swift
+++ 37.playground/Contents.swift
@@ -448,32 +448,27 @@ func bubbleSort() -> [Element] {
 
 extension Array where Element: Comparable{
 
-    func InsertionSortWith() -> [Element]{
-    
-        guard count > 1 else {
-            return self
-        }
+    func insertionSort() -> [Element] {
         
-        var returnValue = [Element]()
-    
-        for unsorted in self {
-            if returnValue.count == 0 {
-                returnValue.append(unsorted)
-            } else {
-                var placed = false
-                
-                for (index, sorted) in returnValue.enumerated() {
-                    if unsorted < sorted {
-                        returnValue.insert(unsorted, at: index)
-                        placed = true
-                        break
-                    }
-                }
-                
-                if !placed {
-                    returnValue.append(unsorted)
-                }
+        guard count > 1 else { return self }
+        
+        var returnValue = self
+        
+        for i in 1 ..< count {
+            var currentItemIndex = i
+            
+            // take a copy of the current item
+            let itemToPlace = returnValue[currentItemIndex]
+            
+            // keep going around until we're at the start of the array or find an item that's greater or equal to us
+            while currentItemIndex > 0 && itemToPlace < returnValue[currentItemIndex - 1] {
+                // move this item to the right
+                returnValue[currentItemIndex] = returnValue[currentItemIndex - 1]
+                currentItemIndex -= 1
             }
+            
+            // place our item into its newly sorted place
+            returnValue[currentItemIndex] = itemToPlace
         }
         
         return returnValue

416de6e1e99f406ea8a33c7350586e9d13dbc0a6
Sara <saraahmad1114@yahoo.com>
2017-07-16 16:20:43 -0400

    added question

--- 37.playground/Contents.swift
+++ 37.playground/Contents.swift
@@ -510,6 +510,37 @@ areTwoValuesIsomorphic(firstValue: "clap", secondValue: "slap")
 
 //58. “Write a function that accepts a string containing the characters (, [, {, <, >, }, ], and ) in any arrangement and frequency. It should return true if the brackets are opened and closed in the correct order, and if all brackets are closed. Any other input should false”
 
+func isStringBalancedWithCharacters(input: String) -> Bool {
+    
+    let validBrackets = "([{<>}])"
+    let validCharacterSet = CharacterSet(charactersIn: validBrackets).inverted
+    guard input.rangeOfCharacter(from: validCharacterSet) == nil else { return false }
+    
+    let matchingBrackets: [Character: Character] = ["(": ")", "[": "]", "{": "}", "<": ">"]
+    var usedBrackets = [Character]()
+    
+    for bracket in input.characters {
+        if matchingBrackets.keys.contains(bracket) {
+            // this is an opening bracket
+            usedBrackets.append(bracket)
+        } else {
+            // this is a closing bracket – try to pull off our previous open
+            if let previousBracket = usedBrackets.popLast() {
+                if matchingBrackets[previousBracket] != bracket {
+                    // if they don't match, this is a bad string
+                    return false
+                }
+            } else {
+
+                // we don't have an opening bracket, this is a bad string
+                return false
+            }
+        }
+    }
+    
+    return usedBrackets.count == 0
+}
+
 
 
 
